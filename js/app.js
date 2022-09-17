2d84d5213153c4383bc5c7e8ca6e60917e5a0b27
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2022-09-17 15:44:46 +0300

    Edited: var to let

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+let Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
